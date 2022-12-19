5a98331a3eecde8a71586b7825ef943a9ae074ad
Lawrence137 <lawrencekimani18@gmail.com>
2022-12-19 13:00:23 +0300

    defined variables

--- weather.js
+++ weather.js
@@ -4,4 +4,12 @@ document.getElementById('login-link').addEventListener('click', function(event) 
     document.querySelector('main').style.display = 'none';
   });
 
- 
+  // OpenWeatherMap API. Do not share it publicly.
+const api = '6cfda788a2523a0448bdaf91c4c01eae';
+const iconImg = document.getElementById('weather-icon');
+const loc = document.querySelector('#location');
+const tempC = document.getElementById('temperature');
+const tempF = document.getElementById('weather');
+const desc = document.querySelector('.desc');
+const sunriseDOM = document.querySelector('.sunrise');
+const sunsetDOM = document.querySelector('.sunset');
