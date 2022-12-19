bf5834141f310be170988bdf73c70200a552beb5
Lawrence137 <lawrencekimani18@gmail.com>
2022-12-19 13:31:40 +0300

    set values for password and username

--- weather.js
+++ weather.js
@@ -4,6 +4,42 @@ document.getElementById('login-link').addEventListener('click', function(event) 
     document.querySelector('main').style.display = 'none';
   });
 
+  const validCredentials = [
+    { username: 'user1', password: 'pass1' },
+    { username: 'user2', password: 'pass2' },
+  ]
+    
+  
+  
+  const loginForm = document.getElementById('login-form');
+  const errorMessage = document.getElementById('error-message');
+  const weatherApp = document.getElementById('weather-app');
+  
+  loginForm.addEventListener('submit', (event) => {
+    // Prevent the form from submitting and refreshing the page
+    event.preventDefault();
+    
+  
+    // Get the username and password entered by the user
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    // Check if the entered username and password match a valid set of credentials
+    const isValidCredentials = validCredentials.some(cred => cred.username === username && cred.password === password);
+  
+    if (isValidCredentials) {
+      // Hide the login form and display the weather app
+      loginForm.style.display = 'none';
+      errorMessage.style.display = 'none';
+      weatherApp.style.display = 'block';
+    } else {
+      // Show an error message indicating that the entered username and password are invalid
+      errorMessage.textContent = 'Invalid username or password';
+      errorMessage.style.display = 'block';
+    }
+  
+  });
+
   document.getElementById('signup-link').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the link from being followed
     document.querySelector('.signup-container').style.display = 'block';
